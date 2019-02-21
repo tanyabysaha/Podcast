59572f0e65b8cdf9c25bb3878c4616fe5070e47d
Tetiana Bysaha <tanyabysaha@gmail.com>
2019-02-21 19:52:36 +0200

    Added pagination and search

--- podcast/views.py
+++ podcast/views.py
@@ -1,8 +1,36 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from django.shortcuts import render
-
 from podcast.models import Podcast
 
 
+
+
 def podcasts_list(request):
+
     podcasts = Podcast.objects.all()
-    return render(request, 'podcast/podcast_list.html', {"podcasts": podcasts})
+    query = request.GET.get("q")
+    if query:
+        podcasts = Podcast.objects.filter(Q(pod_title__icontains=query) | Q(description__icontains=query))
+    paginator = Paginator(podcasts, 4)
+    page = request.GET.get('page')
+
+    try:
+        podcasts = paginator.page(page)
+    except PageNotAnInteger:
+        podcasts = paginator.page(1)
+    except EmptyPage:
+        podcasts = paginator.page(paginator.num_pages)
+
+    index = podcasts.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index - 3 if index >= 3 else 0
+    end_index = index + 3 if index <= max_index - 3 else max_index
+    page_range = paginator.page_range[start_index:end_index]
+
+
+
+
+    return render(request, 'podcast/podcast_list.html', {"podcasts": podcasts, "page_range": page_range})
+
+
